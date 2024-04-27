f443f54a92fd7896fdd21b176233fd82b512afb0
Rohit Vedulla <Rvedulla@live.com>
2024-04-26 22:37:49 -0700

    #8 Date

---
+++ Pods/Target Support Files/Pods-Heads Up #2-Heads Up #2UITests/Pods-Heads Up #2-Heads Up #2UITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Heads_Up__2_Heads_Up__2UITests : NSObject
+@end
+@implementation PodsDummy_Pods_Heads_Up__2_Heads_Up__2UITests
+@end
---
+++ Pods/Target Support Files/Pods-Heads Up #2Tests/Pods-Heads Up #2Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Heads_Up__2Tests : NSObject
+@end
+@implementation PodsDummy_Pods_Heads_Up__2Tests
+@end
